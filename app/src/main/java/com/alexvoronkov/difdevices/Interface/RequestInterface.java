582a665f39cb8a5b4af396b3d28cbbbced5bb2b2
AlexandrVoronkov <Qwerton00@gmail.com>
2017-10-04 22:32:22 +0300

    Some changes

--- app/src/main/java/com/alexvoronkov/difdevices/Interface/RequestInterface.java
+++ app/src/main/java/com/alexvoronkov/difdevices/Interface/RequestInterface.java
@@ -5,10 +5,6 @@ import com.alexvoronkov.difdevices.ObjectClass.AndroidVersion;
 import retrofit2.Call;
 import retrofit2.http.GET;
 
-/**
- * Created by Санек on 03.10.2017.
- */
-
 public interface RequestInterface {
 
     @GET("android/jsonandroid")
